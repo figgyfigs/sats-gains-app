f5b5c6088360cae6d65ab24f3ab76f3c16133846
Alan <a.figs@protonmail.com>
2021-03-02 22:07:02 -0800

    Added form validation and a testing message

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests, json
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
+    if request.method == "POST":
+        user_amount = request.form['amount']
+        sats_back = request.form['sats']
+        print("The user spent: " + user_amount + " and got " + sats_back + " in sats back")
     return render_template('index.html')
 
-price_url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd&include_market_cap=true&\
-                include_24hr_vol=false&include_24hr_change=false&include_last_updated_at=false"
-response = requests.get(price_url)
-response = response.json()
-bitcoin_price = response['bitcoin']['usd']
-print(type(bitcoin_price))
-print(bitcoin_price)
+def get_price():
+    price_url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd&include_market_cap=true&\
+                    include_24hr_vol=false&include_24hr_change=false&include_last_updated_at=false"
+    response = requests.get(price_url)
+    response = response.json()
+    bitcoin_price = response['bitcoin']['usd']
+    return bitcoin_price
+
+get_price()
 
 if __name__ == '__main__':
     app.run(debug=True)
